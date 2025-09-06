f8d7a2516fdd466675c0b569120b2f93a4d0dc9e
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-09-06 23:41:12 +0900

    add simple middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureTokenIsValid;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +16,7 @@ Route::redirect('/redirect', '/');
 Route::get('/posts/{post}/comments/{comment}', function (string $postId, string $commentId) {
     return 'Post ID: ' . $postId . ', Comment ID: ' . $commentId;
 })->whereNumber('post')->whereAlphaNumeric('comment');
+
+Route::get('/protected', function () {
+    return 'Protected Page';
+})->middleware(EnsureTokenIsValid::class);
