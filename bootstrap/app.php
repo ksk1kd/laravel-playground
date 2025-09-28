1f8f5ba74b4c8e4747d351edabfbd9ea400ac02b
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-09-29 02:59:03 +0900

    add sample task sheduling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,8 @@
 
 use App\Http\Middleware\AddContext;
 use App\Http\Middleware\AssignRequestId;
+use App\Jobs\TestJob;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,6 +19,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustHosts(at: fn () => config('app.trusted_hosts'));
         $middleware->append(AssignRequestId::class);
         $middleware->append(AddContext::class);
+    })
+    ->withSchedule(function (Schedule $schedule): void {
+        $schedule->job(TestJob::class)->everyMinute();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
